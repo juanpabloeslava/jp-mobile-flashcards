81768dc76019f1c2c850355eba545bd6a60148f7
Juan Pablo <juan.eslava@ergosign.de>
2021-01-02 16:27:26 +0100

    View: decklist shows dummy decks

--- views/DeckList.js
+++ views/DeckList.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+// data
+import { getInitialData } from '../utils/api' 
 
 const DeckList = () => {
+
+    const decks = getInitialData()
+    console.log('decks: ', decks)
+
     return (
         <View>
-            <Text>Decklist comp</Text>
+            {
+                Object.keys(decks).map( deck => {
+
+                    const { title, questions } = decks[deck]
+
+                    return (
+                        <View key={title} style={styles.question}>
+                            <Text>{title}</Text>
+                            {
+                                questions.map( question => <Text key={question.question}>{question.question}</Text>)
+                            }
+                        </View>
+                    )
+                })
+            }
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    question: {
+        marginBottom: '1rem'
+    }
+  })
 
 export default DeckList
