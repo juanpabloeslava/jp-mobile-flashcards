5bd685763f988f05a5930740413a93658ead21e8
Juan Pablo <juan.eslava@ergosign.de>
2021-01-02 12:03:24 +0100

    utils: added initial blank helper methods

---
+++ utils/helpers.js
@@ -0,0 +1,23 @@
+// initial helper methods
+
+
+// return all of the decks along with their titles, questions, and answers.
+export const getAllDecks = () => {
+    console.log('getAllDecks')
+}
+
+// take in a single id argument and return the deck associated with that id.
+export const getDeck = (id) => {
+    console.log('getDeck')
+}
+
+// take in a single title argument and add it to the decks
+export const saveDeckTitle = (title) => {
+    console.log('saveDeckTitle')
+
+}
+
+// take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+export const addCardToDeck = (title, card) => {
+    console.log('addCardToDeck')
+}
