6a628bc5a5117ee2a7cbb8756534263f29175a39
Juan Pablo <juan.eslava@ergosign.de>
2021-01-05 12:48:59 +0100

    view: NewDeck. Added temporal styles

--- utils/colors.js
+++ utils/colors.js
@@ -4,3 +4,14 @@ export const orange = '#e95635'
 export const blue = '#357ded'
 export const red = '#cc2836'
 export const green = '#78a300'
+
+export const colors = {
+    white: '#fff',
+    purple: '#292477',
+    orange: '#e95635',
+    blue: '#357ded',
+    red: '#cc2836',
+    green: '#78a300',
+    gray: '#757575',
+    lightGray: '#afafaf',
+}
--- views/NewDeck.js
+++ views/NewDeck.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
 // colors and icons
-import { white } from '../utils/colors'
+import { colors } from '../utils/colors'
 // reducers and actions
 import { useDispatch, useSelector } from 'react-redux'
 import { addDeck, addDeckAsync } from '../actions'
@@ -32,17 +32,25 @@ const NewDeck = (props) => {
 
     return (
         <View style={styles.container}>
-            <Text>Deck title:</Text>
-            <TextInput
-                onChangeText={handleChangeText}
-                value={deckTitle}
-            />
+            <Text 
+                // style={styles.title}
+            >
+                Deck title:
+            </Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={handleChangeText}
+                    value={deckTitle}
+                />
+            </View>
             <TouchableOpacity
+                style={styles.submitBtn}
                 onPress={submitDeck}
                 title='submit'
                 placeholder="Enter your deck's title"
             >
-                <Text>submit</Text>
+                <Text style={styles.submitBtnText}>submit</Text>
             </TouchableOpacity>
         </View>
     )
@@ -51,10 +59,43 @@ const NewDeck = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: white,
+        backgroundColor: colors.white,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    inputContainer: {
+        height: 48,
+        width: '100%',
+        margin: 16,
+    },
+    input: {
+        flex: 1,
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: colors.lightGray,
+        marginLeft: 16,
+        marginRight: 16,
+    },
+    title: {
+        fontSize: 24,
+        color: colors.blue
+    },
+    submitBtn: {
+        justifyContent: 'center',
+        minWidth: 220,
+        height: 48,
+        backgroundColor: colors.blue,
+        padding: 8,
+        borderRadius: 4,
+        overflow: 'hidden',
+
+    },
+    submitBtnText: {
+        color: colors.white,
+        textAlign: 'center'
+    }
+
 })
 
 export default NewDeck
