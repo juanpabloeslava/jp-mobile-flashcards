8b35a91f0b16522a052d163e9d82d2e25a9f59ef
Juan Pablo <juan.eslava@ergosign.de>
2021-01-07 11:19:55 +0100

    view: Quiz. Gets decks and deck info from store

--- views/Quiz.js
+++ views/Quiz.js
@@ -6,18 +6,35 @@ import { StyleSheet, Text, View, KeyboardAvoidingView, TouchableOpacity } from '
 // colors and icons
 import { colors } from '../utils/colors'
 // reducers and actions
-import { useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { addCardAsync } from '../actions'
 // comps
 import ActionButton from '../components/ActionButton'
 
-const Quiz = () => {
+const Quiz = (props) => {
+
+    const { navigation } = props
+    const deckID = props.route.params.deckID
+
+    const decks = useSelector(state => state.decks)
+    const deck = decks[deckID]
+    console.log('decks from state in Quiz comp:', decks)
+    console.log('quiz on deck:', deck)
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text> Quiz view </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.white,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
 
 export default Quiz;
