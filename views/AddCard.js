a40ab59abf8430781b03edc74aee76de69290c0a
Juan Pablo <juan.eslava@ergosign.de>
2021-01-08 12:29:31 +0100

    views: addCard. added error handling functionality

--- views/AddCard.js
+++ views/AddCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, TextInput, KeyboardAvoidingView, View } from 'react-native'
+import { StyleSheet, Text, TextInput, KeyboardAvoidingView, View, Platform } from 'react-native'
 // navigation
 import { CommonActions } from '@react-navigation/native'
 // colors and icons
@@ -16,9 +16,12 @@ const AddCard = (props) => {
 
     // local state and dispatch
     const dispatch = useDispatch()
-    const [question, setQuestion] = useState('');
-    const [answer, setAnswer] = useState('');
-    const [correctAnswer, setCorrectAnswer] = useState(true);
+    const [question, setQuestion] = useState('')
+    const [answer, setAnswer] = useState('')
+    const [correctAnswer, setCorrectAnswer] = useState(true)
+    // error handling
+    const [questionRequired, setQuestionRequired] = useState(false)
+    const [answerRequired, setAnswerRequired] = useState(false);
 
     // current deck
     const { navigation } = props
@@ -28,6 +31,31 @@ const AddCard = (props) => {
     const handleChangeQuestion = text => setQuestion(text)
     const handleChangeAnswer = text => setAnswer(text)
     const handleChangeCorrectAnswer = value => setCorrectAnswer(value)
+
+    const checkAnswer = () => {
+        console.log('checking input on card')
+        
+        const questionNoWhitespace = question.replace(/\s/g, '')
+        const answerNoWhitespace = answer.replace(/\s/g, '')
+        
+        let failed = false
+        
+        if (!questionNoWhitespace.length) {
+            setQuestionRequired(true)
+            failed = true
+        } 
+        else { setQuestionRequired(false) }
+
+        if (!answerNoWhitespace.length) {
+            setAnswerRequired(true)
+            failed = true
+        } 
+        else { setAnswerRequired(false) }
+        
+        if (failed) return 
+        
+        submitCard()
+    }
 
     const submitCard = () => {
         console.log('card was submitted')
@@ -48,28 +76,35 @@ const AddCard = (props) => {
     ]
 
     return (
-        <KeyboardAvoidingView behavior='padding' style={styles.container}>
-            <Text>
+        <KeyboardAvoidingView behavior={Platform.Os == 'ios' ? 'padding' : 'height'} style={styles.container}>
+            <Text style={styles.title}>
                 Question
             </Text>
             <View style={styles.inputContainer}>
                 <TextInput
-                    style={styles.input}
+                    style={ questionRequired ? styles.inputError : styles.input }
                     value={question}
+                    returnKeyType='next'
                     onChangeText={handleChangeQuestion}
                 />
             </View>
-            <Text>
+            {questionRequired && (
+                <Text style={styles.inputErrorText}>Please enter your question before submiting</Text>
+            )}
+            <Text style={styles.title}>
                 Answer
             </Text>
             <View style={styles.inputContainer}>
                 <TextInput
-                    style={styles.input}
+                    style={ answerRequired ? styles.inputError : styles.input }
                     value={answer}
                     onChangeText={handleChangeAnswer}
                 />
             </View>
-            <Text style={{ marginBottom: 16}}>
+            {answerRequired && (
+                <Text style={styles.inputErrorText}>Please enter your answer before submiting</Text>
+            )}
+            <Text style={[styles.title, {marginBottom:16}]}>
                 Is this answer correct?
             </Text>
             <RadioForm
@@ -80,7 +115,7 @@ const AddCard = (props) => {
                 onPress={handleChangeCorrectAnswer}
             />
             <ActionButton
-                onPress={submitCard}
+                onPress={checkAnswer}
                 text='Submit card'
                 color={colors.blue}
             />
@@ -94,6 +129,11 @@ const styles = StyleSheet.create({
         backgroundColor: colors.white,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        color: colors.blue,
+        // margin: 8
     },
     inputContainer: {
         height: 48,
@@ -109,6 +149,21 @@ const styles = StyleSheet.create({
         marginLeft: 16,
         marginRight: 16,
     },
+    inputError: {
+        flex: 1,
+        padding: 8,
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: colors.red,
+        marginLeft: 16,
+        marginRight: 16,
+    },
+    inputErrorText: {
+        marginTop: -12,
+        marginBottom: 20,
+        color: colors.red,
+        fontSize: 14,
+    }
 })
 
 export default AddCard
